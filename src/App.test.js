47a752c7f1c1fe845e6f853b5fe3f9a480067551
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-10-30 16:54:04 -0600

    Delete any unnecessary file, asset or code after bootstraping with create-react-app tool.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Math magicians calculator text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Math magicians calculator/i);
   expect(linkElement).toBeInTheDocument();
 });
