32d52a82e07320e533caac4a9658d5f99d7f3fbf
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-10-31 22:09:57 -0600

    Implement Display component, define Proptypes and dedefault props.

--- src/components/Display/index.js
+++ src/components/Display/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import Proptypes from 'prop-types';
 import './index.css';
 
-const Display = () => <div>Display</div>;
+const Display = ({ operationResult = '0' }) => (
+  <div>{operationResult}</div>
+);
+
+Display.prototypes = {
+  operationResult: Proptypes.string,
+};
+
+Display.defaultProps = {
+  operationResult: '0',
+};
 
 export default Display;
