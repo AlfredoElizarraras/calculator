41b68ac6d1b92de2f50e3e091ca34f90ea6701b4
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-11-01 09:38:21 -0600

    Style button component and fill props in ButtonPanel component.

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,16 +2,25 @@ import React from 'react';
 import Proptypes from 'prop-types';
 import './index.css';
 
-const Button = ({ btnName = '' }) => (
-  <button type="button">{btnName}</button>
-);
+const Button = ({ btnName = '', color = '', wide = false }) => {
+  const cssClass = `btn btn-bg-color ${wide ? 'w-50' : 'w-25'}`;
+  return (
+    <button type="button" className={cssClass} style={{ backgroundColor: color }}>
+      {btnName}
+    </button>
+  );
+};
 
 Button.prototypes = {
   btnName: Proptypes.string,
+  className: Proptypes.string,
+  wide: Proptypes.bool,
 };
 
 Button.defaultProps = {
   btnName: '',
+  className: '',
+  wide: false,
 };
 
 export default Button;
--- src/components/ButtonPanel/index.js
+++ src/components/ButtonPanel/index.js
@@ -5,32 +5,32 @@ import './index.css';
 const ButtonPanel = () => (
   <div className="buttonPanel flex flex-column">
     <div className="group">
-      <Button btnName="AC" />
-      <Button btnName="+/-" />
-      <Button btnName="%" />
+      <Button btnName="AC" color="#dfdfdf" />
+      <Button btnName="+/-" color="#dfdfdf" />
+      <Button btnName="%" color="#dfdfdf" />
       <Button btnName="÷" />
     </div>
     <div className="group">
-      <Button btnName="7" />
-      <Button btnName="8" />
-      <Button btnName="9" />
+      <Button btnName="7" color="#dfdfdf" />
+      <Button btnName="8" color="#dfdfdf" />
+      <Button btnName="9" color="#dfdfdf" />
       <Button btnName="X" />
     </div>
     <div className="group">
-      <Button btnName="4" />
-      <Button btnName="5" />
-      <Button btnName="6" />
+      <Button btnName="4" color="#dfdfdf" />
+      <Button btnName="5" color="#dfdfdf" />
+      <Button btnName="6" color="#dfdfdf" />
       <Button btnName="-" />
     </div>
     <div className="group">
-      <Button btnName="1" />
-      <Button btnName="2" />
-      <Button btnName="3" />
+      <Button btnName="1" color="#dfdfdf" />
+      <Button btnName="2" color="#dfdfdf" />
+      <Button btnName="3" color="#dfdfdf" />
       <Button btnName="+" />
     </div>
     <div className="group">
-      <Button btnName="0" />
-      <Button btnName="." />
+      <Button btnName="0" color="#dfdfdf" wide="true" />
+      <Button btnName="." color="#dfdfdf" />
       <Button btnName="=" />
     </div>
   </div>
