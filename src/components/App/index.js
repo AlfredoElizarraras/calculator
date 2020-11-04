2ab502d6d6b2f83f4f5ceacd72a013c469c70ca0
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-11-03 22:43:50 -0600

    Change the app component to be a stateful component, and implement clickHandler to pass props to Display and ButtonPanel component.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './index.css';
 import Display from '../Display';
 import ButtonPanel from '../ButtonPanel';
+import calculate from '../../logic/calculate';
 
-const App = () => (
-  <div className="app flex flex-column">
-    <Display />
-    <ButtonPanel />
-  </div>
-);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(buttonName) {
+    const { total, next, operation } = calculate(this.state, buttonName);
+    this.setState({
+      total,
+      next,
+      operation,
+    });
+  }
+
+  render() {
+    const { total, next, operation } = this.state;
+    const result = next && operation
+      ? `${total} ${operation} ${next}`
+      : `${total}`;
+    return (
+      <div className="app flex flex-column">
+        <Display operationResult={result} />
+        <ButtonPanel clickHandler={this.handleClick} />
+      </div>
+    );
+  }
+}
 
 export default App;
