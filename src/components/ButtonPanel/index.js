94bb0128a28442ecdd31169a665fad59a1f04da2
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-11-03 22:50:07 -0600

    Pass clickHandler function to each button in ButtonPanel Component.

--- src/components/ButtonPanel/index.js
+++ src/components/ButtonPanel/index.js
@@ -1,39 +1,46 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
+import Proptypes from 'prop-types';
 import Button from '../Button';
 import './index.css';
 
-const ButtonPanel = () => (
+const ButtonPanel = ({ clickHandler }) => (
   <div className="buttonPanel flex flex-column">
     <div className="group">
-      <Button btnName="AC" color="#dfdfdf" />
-      <Button btnName="+/-" color="#dfdfdf" />
-      <Button btnName="%" color="#dfdfdf" />
-      <Button btnName="÷" />
+      <Button onClick={clickHandler} btnName="AC" color="#dfdfdf" />
+      <Button onClick={clickHandler} btnName="+/-" color="#dfdfdf" />
+      <Button onClick={clickHandler} btnName="%" color="#dfdfdf" />
+      <Button onClick={clickHandler} btnName="÷" />
     </div>
     <div className="group">
-      <Button btnName="7" color="#dfdfdf" />
-      <Button btnName="8" color="#dfdfdf" />
-      <Button btnName="9" color="#dfdfdf" />
-      <Button btnName="X" />
+      <Button onClick={clickHandler} btnName="7" color="#dfdfdf" />
+      <Button onClick={clickHandler} btnName="8" color="#dfdfdf" />
+      <Button onClick={clickHandler} btnName="9" color="#dfdfdf" />
+      <Button onClick={clickHandler} btnName="X" />
     </div>
     <div className="group">
-      <Button btnName="4" color="#dfdfdf" />
-      <Button btnName="5" color="#dfdfdf" />
-      <Button btnName="6" color="#dfdfdf" />
-      <Button btnName="-" />
+      <Button onClick={clickHandler} btnName="4" color="#dfdfdf" />
+      <Button onClick={clickHandler} btnName="5" color="#dfdfdf" />
+      <Button onClick={clickHandler} btnName="6" color="#dfdfdf" />
+      <Button onClick={clickHandler} btnName="-" />
     </div>
     <div className="group">
-      <Button btnName="1" color="#dfdfdf" />
-      <Button btnName="2" color="#dfdfdf" />
-      <Button btnName="3" color="#dfdfdf" />
-      <Button btnName="+" />
+      <Button onClick={clickHandler} btnName="1" color="#dfdfdf" />
+      <Button onClick={clickHandler} btnName="2" color="#dfdfdf" />
+      <Button onClick={clickHandler} btnName="3" color="#dfdfdf" />
+      <Button onClick={clickHandler} btnName="+" />
     </div>
     <div className="group">
-      <Button btnName="0" color="#dfdfdf" wide="true" />
-      <Button btnName="." color="#dfdfdf" />
-      <Button btnName="=" />
+      <Button onClick={clickHandler} btnName="0" color="#dfdfdf" wide="true" />
+      <Button onClick={clickHandler} btnName="." color="#dfdfdf" />
+      <Button onClick={clickHandler} btnName="=" />
     </div>
   </div>
 );
 
+Button.prototypes = {
+  clickHandler: Proptypes.func,
+};
+
 export default ButtonPanel;
+/* eslint-enable react/prop-types */
