ad0342b3ef9511a10b0f1ce9ef63233590bf083d
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-11-03 18:48:23 -0600

    Implement calculate, operate files for the calculator logic.

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -3,17 +3,125 @@ import * as operations from './operationalConstants';
 
 const calculate = (calculator, buttonName) => {
   const { total, next, operation } = calculator;
-  let returnValue = null;
+  let returnValue = {
+    total,
+    next: null,
+    operation: null,
+  };
 
-  if (buttonName === operations.CLEAR) {
+  const resetCalculator = () => {
     returnValue = {
       total: 0,
       next: null,
       operation: null,
     };
-  }
+  };
 
-  if (next) {
+  const assignSameValues = () => {
+    returnValue = {
+      total,
+      next,
+      operation,
+    };
+  };
+
+  if (buttonName === operations.CLEAR) {
+    resetCalculator();
+  } else if (Number.isNaN(+buttonName)) {
+    switch (buttonName) {
+      case operations.EQUAL:
+        if (total && next && operation) {
+          returnValue = {
+            total: operate(total, next, operation),
+            next: null,
+            operation: null,
+          };
+        } else {
+          assignSameValues();
+        }
+        break;
+      case operations.PERCENTAGE:
+        if (next) {
+          returnValue = {
+            total,
+            next: operate(next, '0.01', operations.MULTIPLY),
+            operation,
+          };
+        } else if (total) {
+          returnValue = {
+            total: operate(total, '0.01', operations.MULTIPLY),
+            next,
+            operation,
+          };
+        } else {
+          assignSameValues();
+        }
+        break;
+      case operations.PLUS_NEGATIVE:
+        if (next) {
+          returnValue = {
+            total,
+            next: operate(next, '-1', operations.MULTIPLY),
+            operation,
+          };
+        } else if (total) {
+          returnValue = {
+            total: operate(total, '-1', operations.MULTIPLY),
+            next,
+            operation,
+          };
+        } else {
+          assignSameValues();
+        }
+        break;
+      case operations.DOT:
+        if (next) {
+          returnValue = {
+            total,
+            next: operate(next, operations.DOT, operations.JOIN),
+            operation,
+          };
+        } else if (total) {
+          returnValue = {
+            total: operate(total, operations.DOT, operations.JOIN),
+            next,
+            operation,
+          };
+        }
+        break;
+      default:
+        if (buttonName === operations.SUM
+           || buttonName === operations.SUBTRACT
+           || buttonName === operations.MULTIPLY
+           || buttonName === operations.DIVIDE) {
+          if (!next) {
+            returnValue = {
+              total,
+              next,
+              operation: buttonName,
+            };
+          } else if (next && operation) {
+            returnValue = {
+              total: operate(total, next, operation),
+              next: null,
+              operation: buttonName,
+            };
+          }
+        }
+        break;
+    }
+  } else if (!Number.isNaN(+buttonName)) {
+    if (next) {
+      returnValue = {
+        total,
+        next: operate(next, buttonName, operations.JOIN),
+        operation,
+      };
+    } else if (total) {
+      returnValue = {
+        total: operate(total, buttonName, operations.JOIN),
+      };
+    }
   }
 
   return returnValue;
--- src/logic/operate.js
+++ src/logic/operate.js
@@ -2,27 +2,41 @@ import Big from 'big.js';
 import * as operations from './operationalConstants';
 
 const operate = (numberOne, numberTwo, operation) => {
+  let numOne;
+  let numTwo;
+  let result;
+
   if (operation !== operations.JOIN) {
-    numberOne = Big(parseFloat(numberOne));
-    numberTwo = Big(parseFloat(numberTwo));
-  }
-  
-  switch (operation) {
-    case operations.SUM:
-      return Big(numberOne + numberTwo);
-    case operations.SUBTRACT:
-      return Big(numberOne - numberTwo);
-    case operations.MULTIPLY:
-      return Big(numberOne * numberTwo);
-    case operations.DIVIDE:
-      if (numberTwo === 0)
-       return null;
-      else
-        Big(numberOne / numberTwo);
-    case operations.JOIN:
-      return `${numberOne}${numberTwo}`;
+    numOne = Big(parseFloat(numberOne));
+    numTwo = Big(parseFloat(numberTwo));
   }
 
+  switch (operation) {
+    case operations.SUM:
+      result = Big(numOne + numberTwo);
+      break;
+    case operations.SUBTRACT:
+      result = Big(numOne - numTwo);
+      break;
+    case operations.MULTIPLY:
+      result = Big(numOne * numTwo);
+      break;
+    case operations.DIVIDE:
+      if (numTwo === 0) {
+        result = null;
+      } else {
+        result = Big(numOne / numTwo);
+      }
+      break;
+    case operations.JOIN:
+      result = `${numOne}${numTwo}`;
+      break;
+    default:
+      result = null;
+      break;
+  }
+
+  return result;
 };
 
 export default operate;
